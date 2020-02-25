573ae2b5510010e261cdb950ae82f5512616d97b
DanilN1 <36081046+DanilN1@users.noreply.github.com>
2020-02-24 22:20:56 -0700

    Up
    .

---
+++ understandingAsserts/src/test/java/mx/tec/lab/OtherClassTest.java
@@ -0,0 +1,20 @@
+package mx.tec.lab;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class OtherClassTest {
+
+	@Test
+	public void testMultiply() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMultiply_ExceptionIsThrown() {
+		fail("Not yet implemented");
+	}
+
+
+}
