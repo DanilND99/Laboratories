0808b662506a0ca7c2f28916930752ee7d7da611
DanilN1 <36081046+DanilN1@users.noreply.github.com>
2020-03-02 21:12:40 -0700

    LAB 3
    lab

---
+++ usingMocks/src/main/java/mx/tec/lab/DothrakiTranslator.java
@@ -0,0 +1,15 @@
+package mx.tec.lab;
+import java.util.Map;
+import java.util.HashMap;
+public class DothrakiTranslator {
+	Map <String,String> translationMap;
+	public DothrakiTranslator() {
+		translationMap = new HashMap<String,String>();
+	}
+	public void addTranslation(final String word, final String translation) {
+		translationMap.put(word, translation);
+	}
+	public String getTranslation(final String word) {
+		return translationMap.get(word);
+	}
+}
